41d3460bf7cc0563fb32c28a88b4dcc97dead797
Muhammad <muhammadabduh@Muhammads-MacBook-Air.local>
2015-05-19 20:29:34 +0200

    Updated, reading images

--- Project_2/Task2.cpp
+++ Project_2/Task2.cpp
@@ -10,6 +10,8 @@
 #include <opencv2/opencv.hpp>
 #include <dirent.h>
 #include <random>
+#include <fstream>
+
 
 using namespace std;
 using namespace cv;
@@ -18,6 +20,10 @@ vector<int> ninety;
 vector<int> ten;
 vector<string> name_files;
 int num_files;
+Mat_<int> Xtrain;
+int image_width = 19;
+int image_height = 19;
+char filename[20];
 
 int readDir(void){ // initialization
     DIR *dir;
@@ -77,15 +83,38 @@ void classify_images(void){
     for (int i = 0; i < num_files; i++) {
         printf("%d\n", temp.at(i));
     }
+    
+    
 }
 
+void read_images(){
+// read images and save to the matrix as one array
+    Xtrain = Mat_<int>::zeros((int)ninety.size(),(image_width*image_height));
+    string line;
 
+    
+    for (int i = 0; i < (int)ninety.size(); i++) {
+        sprintf(filename, "train/face%05d.pgm",ninety[i]);
+        Mat image_temp = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
+//        imshow("",image_temp);
+        cout << "ninty " << ninety.size() << endl;
+//        cout << "xtrain " << Xtrain.rows << " " << Xtrain.cols << endl;
+//        cout << "im " << image_temp.rows << " " << image_temp.cols << endl;
+        for (int j = 0; j < image_height; j++) {
+            for (int k = 0; k < image_width; k++){
+                cout << i << " " << j*image_width+k << endl;
+                Xtrain[i][j*image_width+k] = image_temp.at<int>(j,k);
+            }
+        }
+    }
+    
+}
 
 int main( int argc, char** argv ) {
     
     readDir();
     classify_images();
-
+    read_images();
     // Now read training images
     // Just need to open files with name stored in vector filename and index stored in vector ninety
     
